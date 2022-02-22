5d161249782ab84521101eb27055dc591ae06cc0
udaya <udayeswari.savana@gmail.com>
2022-02-22 11:03:10 -0500

    variable sample

--- 02.vars/vars.tf
+++ 02.vars/vars.tf
@@ -1,9 +1,45 @@
 #variable "sample" {}
 
 variable "sample" {
-  default = "Newworld"
+  default = "NewHello"
 }
 
-output "sample" {
-  value = var.sample
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
 }
+
+## Single Quotes are not supported in Terraform
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+variable "list" {
+  default = ["Hello", 90, true]
+}
+# List can have values of different data types.
+
+variable "map" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+## Variable values from tfvars
+variable "new" {}
+
+## Value from CLI
+variable "cli" {}
+
+
+variable "shell" {}
