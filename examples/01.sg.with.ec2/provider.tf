a3a63dc27aaeebe1c9a64124d20c025c335d4f60
udaya <udayeswari.savana@gmail.com>
2022-02-22 18:24:54 -0500

    sample example to create ec2 instance

--- examples/01.sg.with.ec2/provider.tf
+++ examples/01.sg.with.ec2/provider.tf
@@ -2,10 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-b62"
-#    key    = "sample/terraform.tfstate"
-#    region = "us-east-1"
-#  }
-#}
