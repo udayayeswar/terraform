b2fc0647b4a0c2e37526ba8d162c1f36730aad11
udaya <udayeswari.savana@gmail.com>
2022-03-11 11:37:57 -0500

    sample example to create ec2 instance

---
+++ examples/02-sg-with-ec2-with-modules/ec2/ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0b90346fbb8e13c09"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+variable "SG_ID"{}
