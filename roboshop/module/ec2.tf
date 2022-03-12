b5608ddf3e589e9b10979096f78fc60044903b40
udaya <udayeswari.savana@gmail.com>
2022-03-11 21:19:28 -0500

    sample example to create ec2 instance

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -10,10 +10,10 @@
 
 
 resource "aws_spot_instance_request" "cheap_worker" {
-  ami           = "ami-0b90346fbb8e13c09"
-  instance_type = "t3.micro"
+  ami                    = "ami-0b90346fbb8e13c09"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
-
+  wait_for_fulfilment = true
   tags = {
     Name = var.COMPONENT
   }
