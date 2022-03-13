4551857947edd88b2e1c6156e019639ebf75e1ee
udaya <udayeswari.savana@gmail.com>
2022-03-12 21:26:14 -0500

    sample example to create ec2 instance

--- roboshop/module/route53.tf
+++ roboshop/module/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = "Z05460903OI78EPX9ROOP"
-  name    = var.COMPONENT
+  name    = "${var.COMPONENT[name]}"
   type    = "A"
   ttl     = "300"
   records = [aws_spot_instance_request.cheap_worker.private_ip]
