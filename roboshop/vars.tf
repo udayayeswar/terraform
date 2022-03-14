6e88bcec2c1684050ad190b83c048387bbe303b1
udaya <udayeswari.savana@gmail.com>
2022-03-13 22:05:54 -0400

    sample example to create ec2 instance

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -10,45 +10,45 @@ variable "COMP" {
       port    = 8080
       monitor = "yes"
     },
-#    {
-#      name    = "cart"
-#      port    = 8080
-#      monitor = "yes"
-#    },
-#    {
-#      name    = "user"
-#      port    = 8080
-#      monitor = "yes"
-#    },
-#    {
-#      name    = "shipping"
-#      port    = 8080
-#      monitor = "yes"
-#    },
-#    {
-#      name    = "payment"
-#      port    = 8080
-#      monitor = "yes"
-#    },
-#    {
-#      name    = "mongodb"
-#      port    = 27017
-#      monitor = "no"
-#    },
-#    {
-#      name    = "redis"
-#      port    = 6379
-#      monitor = "no"
-#    },
-#    {
-#      name    = "rabbitmq"
-#      port    = 5672
-#      monitor = "no"
-#    },
-#    {
-#      name    = "mysql"
-#      port    = 3306
-#      monitor = "no"
-#    }
+   {
+      name    = "cart"
+      port    = 8080
+      monitor = "yes"
+    },
+    {
+      name    = "user"
+      port    = 8080
+      monitor = "yes"
+    },
+    {
+      name    = "shipping"
+      port    = 8080
+      monitor = "yes"
+    },
+    {
+      name    = "payment"
+      port    = 8080
+      monitor = "yes"
+    },
+    {
+      name    = "mongodb"
+      port    = 27017
+      monitor = "no"
+    },
+    {
+      name    = "redis"
+      port    = 6379
+      monitor = "no"
+    },
+    {
+      name    = "rabbitmq"
+      port    = 5672
+      monitor = "no"
+    },
+    {
+      name    = "mysql"
+      port    = 3306
+      monitor = "no"
+    }
   ]
 }
