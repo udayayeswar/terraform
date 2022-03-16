f3003e2aa7a49ed84fcaf2270eb3a239cbf0e584
udaya <udayeswari.savana@gmail.com>
2022-03-15 21:45:48 -0400

    sample example to create ec2 instance

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,35 +1,35 @@
-#module "frontend" {
-#  source = "./frontend"
-#}
-#module "cart" {
-#  source = "./cart"
-#}
-#module "mongodb" {
-#  source = "./mongodb"
-#}
-#module "catalogue" {
-#  source = "./catalogue"
-#}
-#module "mysql" {
-#  source = "./mysql"
-#}
-#module "redis" {
-#  source = "./redis"
-#}
-#module "rabbitmq" {
-#  source = "./rabbitmq"
-#}
-#module "payment" {
-#  source = "./payment"
-#}
-#module "shipping" {
-#  source = "./shipping"
-#}
-#module "user" {
-#  source = "./user"
-#}
-module "roboshop" {
-  count     = length(var.COMP)
-  source    = "./module"
-  COMPONENT = var.COMP[count.index]
+module "frontend" {
+  source = "./frontend"
 }
+module "cart" {
+  source = "./cart"
+}
+module "mongodb" {
+  source = "./mongodb"
+}
+module "catalogue" {
+  source = "./catalogue"
+}
+module "mysql" {
+  source = "./mysql"
+}
+module "redis" {
+  source = "./redis"
+}
+module "rabbitmq" {
+  source = "./rabbitmq"
+}
+module "payment" {
+  source = "./payment"
+}
+module "shipping" {
+  source = "./shipping"
+}
+module "user" {
+  source = "./user"
+}
+#module "roboshop" {
+#  count     = length(var.COMP)
+#  source    = "./module"
+#  COMPONENT = var.COMP[count.index]
+#}
