12ea9451f5d34cf78aca1f3cebed0be754b2a551
udaya <udayeswari.savana@gmail.com>
2022-03-15 22:04:52 -0400

    sample example to create ec2 instance

--- roboshop/module/sg.tf
+++ roboshop/module/sg.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "allow_app" {
 
   ingress {
     description = "APPPORT"
-    from_port   = var.COMPONENT
-    to_port     = var.COMPONENT
+    from_port   = var.COMPONENT["port"]
+    to_port     = var.COMPONENT["port"]
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
