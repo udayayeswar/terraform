503e242a9fb0f119c91fb1dc576cf332ce3d886c
udaya <udayeswari.savana@gmail.com>
2022-03-15 22:31:35 -0400

    sample example to create ec2 instance

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,33 +1,33 @@
-module "frontend" {
-  source = "./frontend"
-}
+#module "frontend" {
+#  source = "./frontend"
+#}
 module "cart" {
   source = "./cart"
 }
-module "mongodb" {
-  source = "./mongodb"
-}
-module "catalogue" {
-  source = "./catalogue"
-}
-module "mysql" {
-  source = "./mysql"
-}
-module "redis" {
-  source = "./redis"
-}
-module "rabbitmq" {
-  source = "./rabbitmq"
-}
-module "payment" {
-  source = "./payment"
-}
-module "shipping" {
-  source = "./shipping"
-}
-module "user" {
-  source = "./user"
-}
+#module "mongodb" {
+#  source = "./mongodb"
+#}
+#module "catalogue" {
+#  source = "./catalogue"
+#}
+#module "mysql" {
+#  source = "./mysql"
+#}
+#module "redis" {
+#  source = "./redis"
+#}
+#module "rabbitmq" {
+#  source = "./rabbitmq"
+#}
+#module "payment" {
+#  source = "./payment"
+#}
+#module "shipping" {
+#  source = "./shipping"
+#}
+#module "user" {
+#  source = "./user"
+#}
 #module "roboshop" {
 #  count     = length(var.COMP)
 #  source    = "./module"
