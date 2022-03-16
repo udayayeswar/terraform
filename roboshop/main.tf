aa8e9828cf64be0c13b54c1e9265ec0770836027
udaya <udayeswari.savana@gmail.com>
2022-03-16 14:41:54 -0400

    sample example to create ec2 instance

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,36 +1,36 @@
-module "frontend" {
-  source = "./frontend"
-}
+#module "frontend" {
+#  source = "./frontend"
+#}
 module "cart" {
   source = "./cart"
 }
-module "mongodb" {
-  source = "./mongodb"
-}
-module "catalogue" {
-  source = "./catalogue"
-}
-module "mysql" {
-  source = "./mysql"
-}
-module "redis" {
-  source = "./redis"
-}
-module "rabbitmq" {
-  source = "./rabbitmq"
-}
-module "payment" {
-  source = "./payment"
-}
-module "shipping" {
-  source = "./shipping"
-}
-module "user" {
-  source = "./user"
-}
-module "dispatch" {
-  source = "./dispatch"
-}
+#module "mongodb" {
+#  source = "./mongodb"
+#}
+#module "catalogue" {
+#  source = "./catalogue"
+#}
+#module "mysql" {
+#  source = "./mysql"
+#}
+#module "redis" {
+#  source = "./redis"
+#}
+#module "rabbitmq" {
+#  source = "./rabbitmq"
+#}
+#module "payment" {
+#  source = "./payment"
+#}
+#module "shipping" {
+#  source = "./shipping"
+#}
+#module "user" {
+#  source = "./user"
+#}
+#module "dispatch" {
+#  source = "./dispatch"
+#}
 #module "roboshop" {
 #  count     = length(var.COMP)
 #  source    = "./module"
